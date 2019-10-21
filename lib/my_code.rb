81820cf7a6bfd16d2a14db43d17a2a2cc94c6a5d
Hans Foster <fosterhans@gmail.com>
2019-10-21 22:37:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,7 @@
 def map_to_negativize(source_array) {
   result_array = Array[]
   for element in source_array {
-    result_array.push(source_array[element])
+    result_array.push(source_array[element * -1])
   }
   source_array
 }
