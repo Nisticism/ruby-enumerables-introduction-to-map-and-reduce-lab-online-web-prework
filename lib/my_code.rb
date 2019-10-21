e0aa1ec29eaf8afb76e8a79f5670713d924d1b49
Hans Foster <fosterhans@gmail.com>
2019-10-21 22:41:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,10 +1,9 @@
 # My Code here....
 
-def map_to_negativize(source_array) {
+def map_to_negativize(source_array)
   result_array = []
   for element in source_array {
     result_array.push(source_array[element * -1])
   }
 source_array
 end
-}
