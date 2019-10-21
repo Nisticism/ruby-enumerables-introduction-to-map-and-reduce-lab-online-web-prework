462b0080567f488626f4ce3bf096826713f995fd
Hans Foster <fosterhans@gmail.com>
2019-10-21 23:03:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -35,10 +35,10 @@ result_array
 end
 
 def reduce_to_total(source_array, starting_point)
-  result_array = []
+  total = starting_point
   counter = 0
   while counter < source_array.length
-    result_array.push(source_array[counter] * -1)
+    total += source_array[counter]
     counter += 1
   end
 result_array
