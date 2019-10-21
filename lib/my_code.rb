2fe3f04100e2a3d289fbe54adc68292cda6add35
Hans Foster <fosterhans@gmail.com>
2019-10-21 22:36:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,13 +1,5 @@
 # My Code here....
 
-def map(input[1,2,3]) {
-  result = Array[]
-  for element in input {
-    result.push(input[element * -1])
-  }
-  result
-}
-
 def map_to_negativize(source_array) {
   result_array = Array[]
   for element in source_array {
